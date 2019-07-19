61b1e15d11526bca4b210749b3f2ad6dbccf131a
rob4abcba <rob_abcba@yahoo.com>
2019-07-19 11:36:09 -0700

    import {Provider} & render to connect react&redux

--- src/index.js
+++ src/index.js
@@ -4,47 +4,55 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { createStore } from "redux";
-import allReducer from './reducers';
+import allReducer from "./reducers";
+import { Provider } from "react-redux";
 
 // //STORE -> GLOBALIZED STATE
 
-// //ACTION (human description of what I want to do) INCREMENT
-// const increment = () => {
-//   return {
-//     type: "INCREMENT"
-//   };
-// };
-// const decrement = () => {
-//   return {
-//     type: "DECREMENT"
-//   };
-// };
+//ACTION (human description of what I want to do) INCREMENT
+const increment = () => {
+  return {
+    type: "INCREMENT"
+  };
+};
+const decrement = () => {
+  return {
+    type: "DECREMENT"
+  };
+};
 
-// //REDUCER (computer changes state to accomplish ACTION)
-// const counter = (state = 0, action) => {
-//   switch (action.type) {
-//     case "INCREMENT":
-//       return state + 1;
-//     case "DECREMENT":
-//       return state - 1;
-//   }
-// };
+//REDUCER (computer changes state to accomplish ACTION)
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
 
 // let store = createStore(counter);
-const store = createStore(
-    allReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const myStore = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 // //Display in console
-store.subscribe(() => console.log(store.getState()));
+myStore.subscribe(() => console.log(myStore.getState()));
 
 // //DISPATCH
-store.dispatch(increment());
+myStore.dispatch(increment());
 // store.dispatch(decrement());
 // store.dispatch(decrement());
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={myStore}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
